4764ac9689ff0bc62e88e4fcca59e5d9243609b8
dylanlxlx <islxhere@163.com>
2025-02-18 13:40:09 +0800

    【更新】传感器数据滤波工具类（更新注释）

--- app/src/main/java/com/dylanlxlx/instameasure/utils/SensorFilter.java
+++ app/src/main/java/com/dylanlxlx/instameasure/utils/SensorFilter.java
@@ -1,6 +1,15 @@
 package com.dylanlxlx.instameasure.utils;
 
-// 传感器滤波算法
+
+/**
+ * 传感器滤波算法:<br>
+ * <pre>Y(n)=αX(n) + (1-α)Y(n-1)<br>
+ *   =Y(n-1) + α(X(n)-Y(n-1))<br>
+ * Y(n)：本次滤波输出<br>
+ * X(n)：本次采样输入<br>
+ * Y(n-1)：上次滤波输出<br>
+ * α：滤波系数，取值范围[0,1]
+ */
 public class SensorFilter {
     // 低通滤波：消除高频噪声
     public static float[] lowPass(float[] input, float[] prevOutput, float alpha) {
@@ -12,7 +21,7 @@ public class SensorFilter {
         return output;
     }
 
-    // 计算加速度模长
+    // 计算三维加速度向量模长
     public static float calculateMagnitude(float[] accelerometer) {
         return (float) Math.sqrt(
                 accelerometer[0] * accelerometer[0] +
