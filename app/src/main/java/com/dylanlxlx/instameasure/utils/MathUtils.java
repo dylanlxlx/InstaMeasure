9db75eaaa5b81d1f381d191850309425520dea84
dylanlxlx <islxhere@163.com>
2025-02-18 17:46:56 +0800

    【更新】方向计算工具类（更新注释）

--- app/src/main/java/com/dylanlxlx/instameasure/utils/MathUtils.java
+++ app/src/main/java/com/dylanlxlx/instameasure/utils/MathUtils.java
@@ -1,17 +1,30 @@
 package com.dylanlxlx.instameasure.utils;
 
 // 几何计算工具
+
 import com.dylanlxlx.instameasure.model.TrajectoryPoint;
+
 import java.util.List;
 
 public class MathUtils {
     //TODO:目前仅仅实现了计算规则矩形面积的方法，后续可以继续添加其他不规则几何计算方法
+
+    /**
+     * Shoelace Theorem<br><pre>
+     * 鞋带定理，也称为高斯面积公式<br>
+     * 适用于简单多边形（不交叉的多边形）<br>
+     * 计算公式:<br>
+     * <img src="https://raw.githubusercontent.com/dylanlxlx/ImageRepository/master/InstaMeasure/formula1.png">
+     *
+     * @param points 多边形的各个顶点
+     * @return 多边形面积
+     */
     public static double calculatePolygonArea(List<TrajectoryPoint> points) {
         double area = 0.0;
         int n = points.size();
         for (int i = 0; i < n; i++) {
             TrajectoryPoint p1 = points.get(i);
-            TrajectoryPoint p2 = points.get((i+1)%n);
+            TrajectoryPoint p2 = points.get((i + 1) % n);
             area += p1.getX() * p2.getY() - p2.getX() * p1.getY();
         }
         return Math.abs(area) / 2.0;
